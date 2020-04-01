7cef6abce2033c1e1cb7c11ab811d1f6fed885ef
Angel Plaza Bustamante <angelplazab@gmail.com>
2020-04-01 00:14:20 -0500

    template con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ItemDetalleComponent } from './pages/item-detalle/item-detalle.component';
+import { AboutComponent } from './pages/about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: PortafolioComponent},
+  {path:'item', component: ItemDetalleComponent},
+  {path:'asercade', component: AboutComponent},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes , {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

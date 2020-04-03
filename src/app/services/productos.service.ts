88403391627d0239a68049b68d15a4ecc7b12367
Angel Plaza Bustamante <angelplazab@gmail.com>
2020-04-02 23:45:54 -0500

    template terminado

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProductoInterface } from '../interfaces/producto.interface';
+import { resolve } from 'dns';
+import { rejects } from 'assert';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,27 +19,55 @@ export class ProductosService {
   }
 
   private cargarPorductos() {
-    this.http.get('https://loginapp-1e722.firebaseio.com/productos_idx.json')
-      .subscribe((resp: ProductoInterface[]) => {
-        // console.log(resp);
-        this.productos = resp
-
-        setTimeout(() => {
+    return new Promise((resolve, rejects) => {
+      this.http.get('https://loginapp-1e722.firebaseio.com/productos_idx.json')
+        .subscribe((resp: ProductoInterface[]) => {
+          // console.log(resp);
+          this.productos = resp
           this.cargando = false;
-        }, 2000);
-      })
+          resolve();
+          // setTimeout(() => {
+          //   this.cargando = false;
+          //   resolve();
+          // }, 1500);
+        })
+    });
   }
 
-  public getProducto(id:string){
+  public getProducto(id: string) {
     return this.http.get(`https://loginapp-1e722.firebaseio.com/productos/${id}.json`)
   }
 
-  public buscarProducto(termimo: string){
-    this.productosFiltrado = this.productos.filter(producto =>{
-      return true;
-    })
-    console.log(this.productosFiltrado);
-    
+  public buscarProducto(termino: string) {
+
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarPorductos().then(()=>{
+        // ejectuar despues de tener los productos
+        //aplicar filtro
+        this.filtrarProdutos(termino);
+      })
+    }else{
+      this.filtrarProdutos(termino);
+    }
   }
+
+  private filtrarProdutos(termino: string){
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase(); //PASAMOS A MINUSCULA      
+      this.productos.forEach(prod => {
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+        if(prod.categoria.indexOf(termino)>= 0 || tituloLower.indexOf(termino) >= 0 ){
+          this.productosFiltrado.push(prod);
+        }
+      })
+  }
+
+
+
+
+
+
 
 }
